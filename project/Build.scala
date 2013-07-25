c977b9b59158b93819bbbd66e0d57161f7a865e3
Johan Christian Saarem <johan.saarem@gmail.com>
2013-07-25 14:21:06 +0200

    fixed small error

--- project/Build.scala
+++ project/Build.scala
@@ -11,12 +11,12 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here 
-    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"     
+    // Add your own project settings here        
   )
 
 }
