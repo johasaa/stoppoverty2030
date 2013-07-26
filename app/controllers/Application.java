38609c800855721d0ca0e265daff423908982e9e
Johan Christian Saarem <johan.saarem@gmail.com>
2013-07-26 23:43:17 +0200

    Added first stage input form for signature storage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,44 @@
 package controllers;
 
+import play.data.Form;
+import play.data.validation.Constraints.Email;
+import play.data.validation.Constraints.Required;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.signNoFB;
+
 
 public class Application extends Controller {
   
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	public static class SignNoFacebook{
+		@Required public String firstName;
+		@Required public String lastName;
+		@Required public Email email;
+		@Required public Boolean isPerson;
+		public String group;
+		public Integer numberOfSignatures;
+	}
+	
+    public static Result index() {    	
+        return ok(index.render("Stop Poverty 2030"));
+    }
+    
+    public static Result signPetitionForm() {   
+    	
+    	return ok(signNoFB.render(new Form<Application.SignNoFacebook>(SignNoFacebook.class), "Stop Poverty 2030"));
+    }
+    
+    public static Result saveSignature(){
+    	Form<Application.SignNoFacebook> form = Form.form(SignNoFacebook.class).bindFromRequest();
+    	if(form.hasErrors()) {
+            return badRequest(signNoFB.render(form, ""));
+        } else {
+            SignNoFacebook signatureForm = form.get();
+            return ok(""
+                //hello.render(signatureForm.name, data.repeat, data.color)
+            );
+        }
     }
   
 }
