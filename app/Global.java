0e4bc343ef735e5600ab2f28a54a10d62af750c1
johasaa <johan.saarem@gmail.com>
2014-05-07 23:23:38 +0200

    Adjust global onrequest override

--- app/Global.java
+++ app/Global.java
@@ -1,28 +1,31 @@
 
 
-import play.*;
-import play.libs.F.Promise;
-import play.mvc.*;
+import play.GlobalSettings;
+import play.mvc.Action;
+import play.mvc.Http;
 
 public class Global extends GlobalSettings {
 	
-	private class ActionWrapper extends Action.Simple {
-        public ActionWrapper(Action<?> action) {
-            this.delegate = action;
-        }
-
-        @Override
-        public Promise<SimpleResult> call(Http.Context ctx) throws java.lang.Throwable {
-            Promise<SimpleResult> result = this.delegate.call(ctx);
-            Http.Response response = ctx.response();
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            return result;
-        }
-    }
+//	private class ActionWrapper extends Action.Simple {
+//        public ActionWrapper(Action<?> action) {
+//            this.delegate = action;
+//        }
+//
+//        @Override
+//        public Promise<SimpleResult> call(Http.Context ctx) throws java.lang.Throwable {
+//            Promise<SimpleResult> result = this.delegate.call(ctx);
+//            Http.Response response = ctx.response();
+//            response.setHeader("Access-Control-Allow-Origin", "*");
+//            return result;
+//        }
+//    }
 
     @Override
     public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
-        return new ActionWrapper(super.onRequest(request, actionMethod));
+    	System.out.println("This is overriden: " + request.toString());
+    	Http.Response response = Http.Context.current().response();
+    	response.setHeader("Access-Control-Allow-Origin", "*");
+        return super.onRequest(request, actionMethod);
     }
 
 }
