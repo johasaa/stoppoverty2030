fc82e6c394b9c66b435f7370fb79a88257756173
johasaa <johan.saarem@gmail.com>
2014-05-07 23:33:23 +0200

    Adjust global onrequest neede Promise and new Action

--- app/Global.java
+++ app/Global.java
@@ -1,8 +1,11 @@
 
 
 import play.GlobalSettings;
+import play.libs.F.Promise;
 import play.mvc.Action;
 import play.mvc.Http;
+import play.mvc.SimpleResult;
+import play.mvc.Http.Context;
 
 public class Global extends GlobalSettings {
 	
@@ -23,9 +26,18 @@ public class Global extends GlobalSettings {
     @Override
     public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
     	System.out.println("This is overriden: " + request.toString());
-    	Http.Response response = Http.Context.current().response();
-    	response.setHeader("Access-Control-Allow-Origin", "*");
-        return super.onRequest(request, actionMethod);
+//    	Http.Response response = 
+//    	response.setHeader(Http.Response.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+//        return super.onRequest(request, actionMethod);
+    	return new Action.Simple() {
+			
+			@Override
+			public Promise<SimpleResult> call(Context ctx) throws Throwable {
+				Http.Response response = ctx.response();
+				response.setHeader(Http.Response.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+				return delegate.call(ctx);
+			}
+		};
     }
 
 }
